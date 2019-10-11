9f2a05caf123369f01248599312cd9dee0fb4188
Jan Muehlemann <jan.muehlemann@gmail.com>
2019-10-11 11:44:21 +0200

    rebuild with new parser option

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -20,20 +20,4 @@ describe('i18next translation parser', () => {
    });
  });
 
- describe('basic html parse / stringify - with i18next notation', () => {
-  var cu;
-
-  before(() => {});
-
-  var tests = [
-    {args: ['test {{val}} text {{- encoded}} with {{val, format}} some $t{nesting} help'], expected: 'test {{val}} text {{- encoded}} with {{val, format}} some $t{nesting} help'}
-  ];
-
-  tests.forEach((test) => {
-    it('correctly parse and stringify back ' + JSON.stringify(test.args) + ' args', () => {
-      expect(stringify(parse(test.args[0]))).to.eql(test.expected);
-    });
-  });
-});
-
 });
